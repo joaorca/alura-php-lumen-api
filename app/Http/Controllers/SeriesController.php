8f5a2471aabdf20e0a1a4563f5c61a604c163f2c
João R C Almeida <joaorca@unisuam.edu.br>
2020-05-12 22:53:29 -0300

    Mapeando o nosso banco

--- app/Http/Controllers/SeriesController.php
+++ app/Http/Controllers/SeriesController.php
@@ -2,12 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Serie;
 use Illuminate\Http\Request;
 
 class SeriesController extends Controller
 {
     public function index(Request $request)
     {
-        return ['Supernatural', 'Lost'];
+        return Serie::all();
     }
 }
