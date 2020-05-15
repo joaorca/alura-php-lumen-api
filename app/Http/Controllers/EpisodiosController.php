0f85e7a86c2d2ee14431362ec216e0d734e17328
João R C Almeida <joaorca@unisuam.edu.br>
2020-05-14 23:22:47 -0300

    Controller abstrato

--- app/Http/Controllers/EpisodiosController.php
+++ app/Http/Controllers/EpisodiosController.php
@@ -3,52 +3,11 @@
 namespace App\Http\Controllers;
 
 use App\Episodio;
-use Illuminate\Http\Request;
 
-class EpisodiosController extends Controller
+class EpisodiosController extends BaseController
 {
-    public function index(Request $request)
+    public function __construct()
     {
-        return Episodio::all();
-    }
-
-    public function store(Request $request)
-    {
-        return response()
-            ->json(
-                Episodio::create(['nome' => $request->nome]),
-                201
-            );
-    }
-
-    public function show(int $id)
-    {
-        $episodio = Episodio::find($id);
-        if (is_null($episodio)) {
-            return response()->json('', 204);
-        }
-        return response()->json($episodio);
-    }
-
-    public function update(int $id, Request $request)
-    {
-        $episodio = Episodio::find($id);
-        if (is_null($episodio)) {
-            return response()->json(['erro' => 'Recurso não encontrado'], 404);
-        }
-        $episodio->fill($request->all());
-        $episodio->save();
-
-        return $episodio;
-    }
-
-    public function destroy(int $id)
-    {
-        $qtdRecursosRemovidos = Episodio::destroy($id);
-        if ($qtdRecursosRemovidos === 0) {
-            return response()->json(['erro' => 'Recurso não encontrado'], 404);
-        }
-
-        return response()->json('', 204);
+        $this->classe = Episodio::class;
     }
 }
